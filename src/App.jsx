41c3b8284f02f828f713b8f37593fd2a46453ddb
kaiketorres <kaiketorrea@gmail.com>
2024-11-05 21:34:30 -0300

    nessas alteracoes eu aprendi a utilizar props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
+import Pessoa from './components/pessoa'
 
 export default function App() {
 
@@ -8,8 +9,9 @@ export default function App() {
     <>
       <div className='App'>
         <HelloWorld/>
-        <SayMyName name='kaike'/>
-        <SayMyName name='joao'/>
+        <SayMyName name={'kaike'}/>
+          <SayMyName name={'joao'}/>
+        <Pessoa nome={'kaike'} idade={21} profissao={'ourives'}  />
       </div>
     </>
   )
---
+++ src/components/pessoa.jsx
@@ -0,0 +1,8 @@
+export default function Pessoa( {nome, idade, profissao} ) {
+  return(
+    <div>
+      
+      <p>Meu nome e {nome} tenho {idade} e meu trabalho e {profissao}</p>
+    </div>
+  )
+} 
